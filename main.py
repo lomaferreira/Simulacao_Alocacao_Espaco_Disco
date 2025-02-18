be58b8bc2fc58711fba1bb742bc7b19ac42a1e41
Paloma S Ferreira <103579480+lomaferreira@users.noreply.github.com>
2025-02-18 13:02:44 -0300

    nova estrutura da alocacao continua

--- main.py
+++ main.py
@@ -1,45 +1,68 @@
 import math
+# O vetor é considerado a mémoria e cada elemento um bloco
+class arquivo:
+    def __init__(self,indice, dados, proximo):
+        self.indice=indice
+        self.dados=dados
+        self.proximo=proximo
 
+#def desfragmentacao(blocosMemoria):
 
-def alocacaoContinua(tamanhoBlocosDados, blocosMemoria):
+#Realiza a alocação de espaço por meio da estratégia de First-fit
+def alocacaoContinua(blocosDados, blocosMemoria):
     espacosLivres=[]
     tamanho=len(blocosMemoria)
-    espacoNecessario=math.ceil(tamanhoBlocosDados/tamanho)
+    tamanhoDados=len(blocosDados)
     i=0
     while(i<tamanho):
-        if(len(espacosLivres)<espacoNecessario):
-            if all(elemento == 0 for elemento in blocosMemoria[i]):
+        if(len(espacosLivres)<tamanhoDados):
+            if blocosMemoria[i]==0:
                 espacosLivres.append(i)
             else:
                 espacosLivres=[]
-
+        else:
+            break
         i+=1
-
+    #Ocupa os segmentos de espaços livres contínuos
+    k=0
     for j in espacosLivres:
-        blocosMemoria[j]=
+        blocosMemoria[j]=blocosDados[k]
+        k+=1
 
-# def alocacaoEncadeada():
+    return blocosMemoria
 
+#def alocacaoEncadeada():
 # def alocacaoIndexada():
 
-
-
-
+#def exibirVisualmente(blocoMemoria):
 
 def main():
     blocosMemoria=[]
-    tamanhoBloco=5
     i=5
     while(i>0):
-        blocosMemoria.append([0]*tamanhoBloco)
+        blocosMemoria.append(0)
         i-=1
-    
-    print(blocosMemoria)
+
+    aux=[]
+    j=3
+    index=0
+    prox=0
+    while(j>0):
+        index+=1
+        aux.append(arquivo(prox,'nome', index))
+        j-=1
+        prox+=1
+
+    novoVetor=alocacaoContinua(aux, blocosMemoria)
+    print("###Alocacao continua###")
+    for elm in novoVetor:
+        if(elm!=0):
+            print(f"O indice do aquivo eh : {elm.indice} Dados do arquivo: {elm.dados} Proximo bloco a ser alocado: {elm.proximo}")
+        else:
+            print(f"Dado: {elm}")
+main()
 
 
 
 
 
-
-
-main()    
